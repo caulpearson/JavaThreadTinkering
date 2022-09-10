f79106e65825d3519c3164e3001473920d29349a
Caulfield Pearson <caulpearson@gmail.com>
2022-09-10 00:04:32 -0500

    quick thread tinker by subclassing thread

--- src/javaThreadTinkering/Main.java
+++ src/javaThreadTinkering/Main.java
@@ -1,10 +1,12 @@
 package javaThreadTinkering;
 
-public class Main implements Runnable {
+//public class Main implements Runnable {
+public class Main extends Thread {
 	public void run() {
 		System.out.println("This is printed from a thread");
 	}
 	public static void main(String args[]) {
-		(new Thread(new Main())).start();
+		//(new Thread(new Main())).start();
+		(new Main()).start();
 	}
 }
