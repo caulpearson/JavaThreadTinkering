9ece8e4f0d4211d459a4bafefb40008be9ee1e3e
Caulfield Pearson <caulpearson@gmail.com>
2022-09-10 00:00:09 -0500

    quick runnable tinker

--- src/javaThreadTinkering/Main.java
+++ src/javaThreadTinkering/Main.java
@@ -1,5 +1,10 @@
 package javaThreadTinkering;
 
-public class Main {
-
+public class Main implements Runnable {
+	public void run() {
+		System.out.println("This is printed from a thread");
+	}
+	public static void main(String args[]) {
+		(new Thread(new Main())).start();
+	}
 }
