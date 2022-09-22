2341d5807864d5cbe085311b8b4891ef31d8a3c5
Caulfield Pearson <caulpearson@gmail.com>
2022-09-21 22:20:29 -0500

    ProducerConsumer Producer

--- src/ProducerConsumer/Drop.java
+++ src/ProducerConsumer/Drop.java
@@ -1,5 +1,29 @@
 package ProducerConsumer;
 
 public class Drop {
-
+	private String message;
+	private boolean empty = true;
+	
+	public synchronized String take() {
+		while(empty) {
+			try {
+				wait();
+			} catch(InterruptedException e) {
+				
+			}
+		}
+		empty = true;
+		notifyAll();
+		return message;
+	}
+	public synchronized void put(String message) {
+		while(!empty) {
+			try {
+				wait();
+			} catch(InterruptedException e) {}
+		}
+		empty = false;
+		this.message = message;
+		notifyAll();
+	}
 }
---
+++ src/ProducerConsumer/Producer.java
@@ -0,0 +1,30 @@
+package ProducerConsumer;
+
+import java.util.Random;
+
+public class Producer implements Runnable {
+	private Drop drop;
+	
+	public Producer(Drop drop) {
+		this.drop = drop;
+	}
+	
+	public void run() {
+		String importantInfo[] = {
+				"one",
+				"two",
+				"three",
+				"four"
+		};
+		Random random = new Random();
+		
+		for(int i=0;i<importantInfo.length;i++) {
+			drop.put(importantInfo[i]);
+			try {
+				Thread.sleep(random.nextInt(5000));
+			} catch(InterruptedException e) {}
+		}
+		drop.put("Done");
+		
+	}
+}
